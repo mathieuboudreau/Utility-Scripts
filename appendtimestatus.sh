0c1318d53d92d41cf5c6db3ee8459f4c87f148ce
mathieuboudreau <emb6150@gmail.com>
2016-09-17 16:45:31 -0230

    Write function to append current datetime to eof. Make new file if required

---
+++ appendtimestatus.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Append current datetime to eof. If file does not exist, create it.
+
+# args
+FILE=$1
+
+# functions
+function checkmake(){
+    if ! [ -w "$FILE" ]
+    then
+        > $FILE
+	    echo "$FILE was created."
+    fi
+}
+
+# Verify that file exists, and if it doesn't create it
+checkmake $FILE
+
+if [ -w "$FILE" ]
+then
+    echo $(date) >> $FILE
+fi
